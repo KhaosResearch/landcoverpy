0b4f73440ca353d217d4a3cad24e57abcfc6f93d
jfaldanam <jfaldanam@gmail.com>
2021-11-18 19:26:17 +0000

    Added retireval of products for several date ranges
    Added MGRS coordinate translation to find the product of a given lat/long
    
    Fixed inecessary loops and extract geometry parsing and grouping in functions
    
    Improved docstrings

--- workflow.py
+++ workflow.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from functools import partial
 from pathlib import Path
 
-import numpy as np
 import joblib
+import numpy as np
 import rasterio
 import pandas as pd
 import pyproj
 from minio import Minio
+from mgrs import MGRS
 from pymongo import MongoClient
 from rasterio import mask
 from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
@@ -83,7 +84,8 @@ def project_shape(geom, scs: str = 'epsg:4326', dcs: str = 'epsg:32630'):
 
 def __read_band(band_path: str, mask_geometry: dict = None, scs: str = "epsg:32629", dcs: str = "epsg:4326"):
     '''
-    Reads a Sentinel band as a numpy array
+    Reads a Sentinel band as a numpy array. It scales all bands to a 10m/pxl resolution.
+    If mask_geometry is given, the resturning band is cropped to the mask
     '''
 
     with rasterio.open(band_path) as band_file:
@@ -109,7 +111,7 @@ def __read_band(band_path: str, mask_geometry: dict = None, scs: str = "epsg:326
     if mask_geometry:
         projected_geometry = project_shape(mask_geometry, dcs=destination_crs)
         # Create a temporal memory file to mask the band
-        # This is necessary if the band is previously read to scale its resolution
+        # This is necessary because the band is previously read to scale its resolution
         with rasterio.io.MemoryFile() as memfile:
             with memfile.open(**kwargs) as memfile_band:
                 memfile_band.write(band)
@@ -165,24 +167,27 @@ def pca(data: pd.DataFrame, variance_explained: int = 75):
     return [column_names[id_] for id_ in col_ids]
 
 def _get_corners(geometry: dict):
+    '''
+    Get the coordinates of the 4 corners of the bounding box of a geometry
+    '''
     coordinates = geometry["coordinates"][0] # Takes only the outer ring of the polygon: https://geojson.org/geojson-spec.html#polygon
-    x = [] 
-    y = [] 
+    lon = [] 
+    lat = [] 
     for coordinate in coordinates:
-        x.append(coordinate[0])
-        y.append(coordinate[1])
+        lon.append(coordinate[0])
+        lat.append(coordinate[1])
 
-    max_x = max(x)
-    min_x = min(x)
-    max_y = max(y)
-    min_y = min(y)
+    max_lon = max(lon)
+    min_lon = min(lon)
+    max_lat = max(lat)
+    min_lat = min(lat)
 
     return {
-        "top_left": (min_x, max_y),
-        "top_right": (max_x, max_y),
-        "bottom_left": (min_x, min_y),
-        "bottom_right": (max_x, min_y),
-        } 
+        "top_left": (max_lat, min_lon),
+        "top_right": (max_lat, max_lon),
+        "bottom_left": (min_lat, min_lon),
+        "bottom_right": (min_lat, max_lon),
+    } 
 
 def _get_centroid(geometry: dict):
     '''
@@ -194,59 +199,50 @@ def _get_centroid(geometry: dict):
     Source: https://en.wikipedia.org/wiki/Centroid#Of_a_polygon
     '''
     coordinates = geometry["coordinates"][0] # Takes only the outer ring of the polygon: https://geojson.org/geojson-spec.html#polygon
-    x = [] 
-    y = [] 
+    lon = [] 
+    lat = [] 
     for coordinate in coordinates:
-        x.append(coordinate[0])
-        y.append(coordinate[1])
+        lon.append(coordinate[0])
+        lat.append(coordinate[1])
 
     # Calculate sums
-    sum_x = 0
-    sum_y = 0
+    sum_lon = 0
+    sum_lat = 0
     sum_A = 0
-    for i in range(len(x)-1):
-        sum_x += (x[i] + x[i+1]) * ((x[i] * y[i+1]) - (x[i+1] * y[i]))
-        sum_y += (y[i] + y[i+1]) * ((x[i] * y[i+1]) - (x[i+1] * y[i]))
-        sum_A +=                   ((x[i] * y[i+1]) - (x[i+1] * y[i]))
+    for i in range(len(lon)-1):
+        sum_lon += (lon[i] + lon[i+1]) * ((lon[i] * lat[i+1]) - (lon[i+1] * lat[i]))
+        sum_lat += (lat[i] + lat[i+1]) * ((lon[i] * lat[i+1]) - (lon[i+1] * lat[i]))
+        sum_A +=                         ((lon[i] * lat[i+1]) - (lon[i+1] * lat[i]))
 
     # Calculate area inside the polygon's signed area
     A = sum_A/2
 
     # Calculate centroid coordinates
-    Cx = (1/(6*A)) * sum_x
-    Cy = (1/(6*A)) * sum_y
+    Clon = (1/(6*A)) * sum_lon
+    Clat = (1/(6*A)) * sum_lat
 
-    return (Cx, Cy)
+    return (Clat, Clon)
 
 def _get_mgrs_from_geometry(geometry: dict):
     '''
-    
+    Get the MGRS coordinates for the 4 corners of the bounding box of a geometry
+
     This wont work for geometry bigger than a tile. A chunck of the image could not fit between the products of the 4 corners
     '''
-
+    tiles = set() 
     corners = _get_corners(geometry)
-    # mgrs_corners = get_mgrs(corners)
-    # return set(mgrs_corners)
-    warnings.warn("_get_mgrs_from_geometry is still not implemented, defaulting to 30SUF tile")
-    return set(["30SUF"])
+    for point in corners.values():
+        tiles.add(MGRS().toMGRS(*point, MGRSPrecision=0))
 
+    return tiles
 
-def workflow(training: bool = True):
+def group_polygons_by_tile(geojson_file: Path) -> dict:
     '''
-        Step 1: Load Sentinel-2 imagery
-        (Skip (?))Step 2: Load pre-processed ASTER DEM
-        TODO Step 3: Load River shapefiles (maybe not neccesary)
-        TODO Step 4: Calculate all intermediate bands ???
-        (Skip) Step 5: Terrain correction (Will not be calculated, according to experts it doesn't provide better results)
-        (Skip) Step 6: Index calculation  (If not tc, already calculated)
-        Step 7: Normalization (and convert to tabular structure) (outlier removal(?))
-        Step 8: layer selection
-        Step 9: Train models 
+    Extract all features from a GeoJSON file and group them on the tiles they intersect
     '''
-
-    geojson = read_geojson(Path("forest-db/Forest_DB_SNieves.geojson"))
+    geojson = read_geojson(geojson_file)
     tiles = {} 
-    # TODO Iterate over a set of geojson (the databases may not be equal)
+
     print(f"Querying relevant tiles for {len(geojson['features'])} features")
     for feature in tqdm(geojson["features"]):
         small_geojson = {"type": "FeatureCollection", "features": [feature] }
@@ -266,94 +262,177 @@ def workflow(training: bool = True):
                 "geometry": geometry,
             })
 
+    return tiles
+
+def workflow(training: bool = True):
+    '''
+        Step 1: Load Sentinel-2 imagery
+        (Skip (?))Step 2: Load pre-processed ASTER DEM
+        TODO Step 3: Load River shapefiles (maybe not neccesary)
+        TODO Step 4: Calculate all intermediate bands ???
+        (Skip) Step 5: Terrain correction (Will not be calculated, according to experts it doesn't provide better results)
+        (Skip) Step 6: Index calculation  (If not tc, already calculated)
+        Step 7: Normalization (and convert to tabular structure) (outlier removal(?))
+        Step 8: layer selection
+        Step 9: Train models 
+    '''
+
+    # TODO Iterate over a set of geojson/databases (the databases may not be equal)
+    polygons_per_tile = group_polygons_by_tile(Path("forest-db/Forest_DB_SNieves.geojson"))
+
     # Tiles related to the traininig zone
-    tiles = ["30SUF"] # ["T29SPA", "T29SPB", "T29SPC", "T29SPD", "T29SQA", "T29SQB", "T29SQC", "T29SQD", "T29SQV", "T29TMG", "T29TMH", "T29TMJ", "T29TNG", "T29TNH", "T29TNJ", "T29TPE", "T29TPF", "T29TPG", "T29TPH", "T29TPJ", "T29TQE", "T29TQF", "T29TQG", "T29TQH", "T29TQJ", "T30STE", "T30STF", "T30STG", "T30STH", "T30STJ", "T30SUE", "T30SUF", "T30SUG", "T30SUH", "T30SUJ", "T30SVF", "T30SVG", "T30SVH", "T30SVJ", "T30SWF", "T30SWG", "T30SWH", "T30SWJ", "T30SXF", "T30SXG", "T30SXH", "T30SXJ", "T30SYG", "T30SYH", "T30SYJ", "T30TTK", "T30TTL", "T30TTM", "T30TUK", "T30TUL", "T30TUM", "T30TUN", "T30TUP", "T30TVK", "T30TVL", "T30TVM", "T30TVN", "T30TVP", "T30TWK", "T30TWL", "T30TWM", "T30TWN", "T30TWP", "T30TXK", "T30TXL", "T30TXM", "T30TXN", "T30TXP", "T30TYK", "T30TYL", "T30TYM", "T30TYN", "T31SBC", "T31SBD", "T31TBE", "T31TBF", "T31TBG", "T31TCE", "T31TCF", "T31TCG", "T31TCH", "T31TDF", "T31TDG", "T31TDH", "T31TEG", "T31TEH"] 
-    bands = ["AOT_10m", "B01_60m", "B02_10m"] #, "B03_10m", "B04_10m", "B05_20m", "B06_20m", "B07_20m", "B08_10m", "B09_60m", "B11_20m", "B12_20m", "B8A_20m", "WVP_10m"] # Removed , "SCL_20m"
+    tiles = polygons_per_tile.keys() 
+    # This parameters should be coming from somewhere else
+    bands = ["AOT_10m", "B01_60m", "B02_10m", "B03_10m", "B04_10m", "B05_20m", "B06_20m", "B07_20m", "B08_10m", "B09_60m", "B11_20m", "B12_20m", "B8A_20m", "WVP_10m"] # Removed , "SCL_20m"
+    spring_start = datetime(2020, 3, 1)
+    spring_end = datetime(2020, 3, 31)
+    summer_start = datetime(2020, 7, 1)
+    summer_end = datetime(2020, 7, 31)
+    autumn_start = datetime(2020, 10, 1)
+    autumn_end = datetime(2020, 10, 31)
+    
     # Step 1 
     minio_client = get_minio()
     mongo_col = get_mongo_collection()
     
     # Model input
-    train_data = None
+    train_df = None
 
     # Search product metadata in Mongo
     for tile in tiles:
-        print(f"Working through tiles {tile}")
-        product_data = mongo_col.find({"title": {"$regex": f"_T{tile}_"}})
+        for geometry_id in (0,4, 69): # Take some ids to speed up the demo  
+            print(f"Working through tiles {tile}")
+            # TODO Filter por fecha, take 3 of every year y añadir todas las bandas
+            # Query sample {"title": {"$regex": "_T30SUF_"}, "date": {"$gte": ISODate("2020-07-01T00:00:00.000+00:00"),"$lte": ISODate("2020-07-31T23:59:59.999+00:00")}}
+            product_data_spring = mongo_col.find({
+                "title": {
+                    "$regex": f"_T{tile}_"
+                },
+                "date": {
+                    "$gte": spring_start,
+                    "$lte": spring_end,
+                },
+            })
+            product_data_summer = mongo_col.find({
+                "title": {
+                    "$regex": f"_T{tile}_"
+                },
+                "date": {
+                    "$gte": summer_start,
+                    "$lte": summer_end,
+                },
+            })
+            product_data_autumn = mongo_col.find({
+                "title": {
+                    "$regex": f"_T{tile}_"
+                },
+                "date": {
+                    "$gte": autumn_start,
+                    "$lte": autumn_end,
+                },
+            })
 
-        for product in product_data:
-            year = product["date"].strftime("%Y")
-            month = product["date"].strftime("%B")
-            minio_dir = year + "/" + month + "/"
-            bands_dir = minio_dir +  product["title"] + "/raw/"
-            temp_product_folder = settings.TMP_DIR + "/" + product["title"] + ".SAFE"
+            product_per_season = {
+                "spring": list(product_data_spring),
+                "summer": list(product_data_summer),
+                "autumn": list(product_data_autumn),
+            } 
 
-            # Create product dataframe
-            single_product_df = None
+            season_df = None
 
-            # Get all sentinel bands
-            for band_name in bands:
-                band_file = band_name + ".jp2"
-                print("Reading band: ->", settings.MINIO_BUCKET_NAME, bands_dir + band_file, temp_product_folder + "/" + band_file)
-                
-                minio_client.fget_object(
-                    settings.MINIO_BUCKET_NAME,
-                    bands_dir + band_file,
-                    temp_product_folder + "/" + band_file,
-                )
+            for season, products in product_per_season.items():
+                if len(products) == 1:
+                    product = products[0]
+                else:
+                # TODO integrate the making of a composite
+                # TODO Should the composites be made before hand?  
+                # product = _make_composite(products)
+                    warnings.warn(f"{len(products)} products available for {season}, but composites not implemented, taking only first")
+                    product = products[0]
 
-                # Once all bands are loaded, they need to be passed to feature selection (PCA, regression, matusita)
-                band = __read_band(temp_product_folder + "/" + band_file, mask_geometry = geometry)
+                year = product["date"].strftime("%Y")
+                month = product["date"].strftime("%B")
+                minio_dir = year + "/" + month + "/"
+                bands_dir = minio_dir +  product["title"] + "/raw/"
+                temp_product_folder = settings.TMP_DIR + "/" + product["title"] + ".SAFE"
+
+                # Create product dataframe
+                single_product_df = None
+
+                # Get all sentinel bands
+                for band_name in bands:
+                    band_file = band_name + ".jp2"
+                    print("Reading band: ->", settings.MINIO_BUCKET_NAME, bands_dir + band_file, temp_product_folder + "/" + band_file)
+                    
+                    minio_client.fget_object(
+                        settings.MINIO_BUCKET_NAME,
+                        bands_dir + band_file,
+                        temp_product_folder + "/" + band_file,
+                    )
+
+                    # Once all bands are loaded, they need to be passed to feature selection (PCA, regression, matusita)
+                    band = __read_band(temp_product_folder + "/" + band_file, mask_geometry = polygons_per_tile[tile][geometry_id]["geometry"])
+                    
+                    normalized_band = normalize(band)
+                    
+                    band_df = pd.DataFrame({f"{season}_{band_name}": normalized_band.flatten()})
+                    
+                    band_df= band_df.dropna()
+                    
+                    if single_product_df is None:
+                        single_product_df = band_df
+                    else: 
+                        single_product_df = pd.concat([single_product_df, band_df], axis=1)
                 
-                normalized_band = normalize(band)
-                
-                band_df = pd.DataFrame({band_name: normalized_band.flatten()})
-                
-                band_df= band_df.dropna()
-                
-                if single_product_df is None:
-                    single_product_df = band_df
+                if season_df is None:
+                    season_df = single_product_df
                 else: 
-                    single_product_df = pd.concat([single_product_df, band_df], axis=1)
-            
+                    season_df = pd.concat([season_df, single_product_df], axis=1)
+
             if training:
                 # Add classification label
-                single_product_df["class"] = classification_label
+                season_df["class"] = polygons_per_tile[tile][geometry_id]["label"]
 
-            if train_data is None:
-                train_data = single_product_df
+            if train_df is None:
+                train_df = season_df
             else: 
-                train_data = pd.concat([train_data, single_product_df], axis=0)
-                break
+                train_df = pd.concat([train_df, season_df], axis=0)
+    print(train_df.head())  
+    train_df.to_csv("dataset.csv")
+    # Temporal code to load premade dataset for training 
+    # train_df = pd.read_csv("dataset2.csv")
+    # train_df = train_df.drop("Unnamed: 0", axis=1)
+    # print(train_df.head())
+    if training:
+        # Prepare data for training
+        x_train_data = train_df.drop("class", axis=1)
+        y_train_data = train_df["class"] 
+
+        # Filter bands according to PCA, matusita etc
+        pc_columns = pca(x_train_data)
+        print(pc_columns)
+        reduced_x_train_data = x_train_data[pc_columns]
         
-        if training:
-            # Prepare data for training
-            x_train_data = train_data.drop("class", axis=1)
-            y_train_data = train_data["class"] 
-
-            # Filter bands according to PCA, matusita etc
-            pc_columns = pca(x_train_data)
-            print(pc_columns)
-            reduced_x_train_data = x_train_data[pc_columns]
-            
-            # Train model 
-            clf = RandomForestClassifier()
-            clf.fit(reduced_x_train_data, y_train_data)
-            joblib.dump(clf, 'model.pkl', compress=1)
-        else: # Prediction
-            
-            reduced_predict_data = train_data[['B02_10m', 'AOT_10m']]
-            clf = joblib.load('model.pkl')
-            results = clf.predict(reduced_predict_data)
-            print(results)
-
+        # Train model 
+        clf = RandomForestClassifier()
+        clf.fit(reduced_x_train_data, y_train_data)
+        joblib.dump(clf, 'model.pkl', compress=1)
+    else: # Prediction
         
-        print("STOP")
-        break
-
+        reduced_predict_data = train_df[['B02_10m', 'AOT_10m']] # This should be coming from the PCA used during training
+        clf = joblib.load('model.pkl')
+        results = clf.predict(reduced_predict_data)
+        print(results)
 
 if __name__ == '__main__':
+    import time
+    start = time.time()
     print("Training")
     workflow(training=True)
-    print("Testing")
-    workflow(training=False)
+    end1 = time.time()
+    print('Training function took {:.3f} ms'.format((end1-start)*1000.0))
+    # print("Testing")
+    # workflow(training=False)
+    # end2 = time.time()
+    # print('Predict function took {:.3f} ms'.format((end2-end1)*1000.0))
+    # print('Workflow in total took {:.3f} ms'.format((end2-start)*1000.0))
