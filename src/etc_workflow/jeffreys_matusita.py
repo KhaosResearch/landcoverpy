bff7edbfe79639eeb66fb0ce6d409637bf5161b7
Antonio Manuel Burgueño Romero <ambrbr@uma.es>
2022-07-12 09:00:12 +0000

    Added plot option for jeffreys-matusita analysis
    Methods are now documented

--- src/etc_workflow/jeffreys_matusita.py
+++ src/etc_workflow/jeffreys_matusita.py
@@ -1,19 +1,44 @@
 from os.path import join
 
 from itertools import combinations
+from matplotlib import pyplot as plt
 import numpy as np
 from numpy.lib.function_base import cov
 import pandas as pd
+import seaborn as sns
 
 from etc_workflow.config import settings
 from etc_workflow.utils import _get_minio, _safe_minio_execute
 
 def _jeffreys_matusita_distance(mu1, sigma1, mu2, sigma2):
+    """
+    Computes the Jeffreys-Matusita distance between two distributions `P` and `Q`.
+    Parameters:
+        mu1 (np.ndarray) : Mean vector of `P`.
+        sigma1 (np.ndarray) : Covariance matrix of `P`.
+        mu2 (np.ndarray) : Mean vector of `Q`.
+        sigma2 (np.ndarray) : Covariance matrix of `Q`.
+
+    Returns:
+        jmd (int) : Jeffreys-Matusita distanc.
+
+    """
     jmd = np.sqrt(2*(1-np.exp(-_bhattacharyya_distance(mu1, sigma1, mu2, sigma2))))
-    return jmd*np.sqrt(2)
+    return jmd
 
 def _bhattacharyya_distance(mu1, sigma1, mu2, sigma2):
+    """
+    Computes the Bhattarcharyya distance between two distributions `P` and `Q`.
+    Parameters:
+        mu1 (np.ndarray) : Mean vector of `P`.
+        sigma1 (np.ndarray) : Covariance matrix of `P`.
+        mu2 (np.ndarray) : Mean vector of `Q`.
+        sigma2 (np.ndarray) : Covariance matrix of `Q`.
 
+    Returns:
+        db (int) : Bhattarcharyya distance.
+
+    """
     sigma = np.mean([sigma1,sigma2], axis=0)
     
     ds = np.linalg.slogdet(sigma)[1]
@@ -24,6 +49,17 @@ def _bhattacharyya_distance(mu1, sigma1, mu2, sigma2):
 
 
 def _squared_mahalanobis_distance(u, v, sigma):
+    """
+    Computes the squared Mahalanobis distance between a point and a distribution `Q`.
+    Parameters:
+        u (np.ndarray) : A point located in the same dimension space than `Q`
+        v (np.ndarray) : Mean vector of `Q`.
+        value2 (np.ndarray) : Covariance matrix of `Q`.
+
+    Returns:
+        m (int) : Squared Mahalanobis distance between a point `u` to a distribution defined by mean vector `v` and covariance matrix `sigma`.
+
+    """
     delta = u - v
     try:
         inv_sigma = np.linalg.inv(sigma)
@@ -37,6 +73,14 @@ def jeffreys_matusita_analysis(
     out_path: str,
     is_forest: bool = False,
 ):
+    """
+    Computes the Jeffreys-Matusita  distance for all class combinations in `input_Dataset`. The distance is multiplied by sqrt(2) in order to map the distance to 0-2.
+    If J-M distance is lower than 1.9 between two different classes, it is considered that those classes are not separable with the current attributes.
+    Parameters:
+        input_dataset (str) : Name of the dataset stored in MinIO.
+        out_path (str) : Local folder where a csv file and a plot will be stored.
+        is_forest (bool) : Indicate if classes used are those in land cover or in forest classification.
+    """
 
     dataset_path = join(settings.TMP_DIR, input_dataset)
 
@@ -74,11 +118,22 @@ def jeffreys_matusita_analysis(
     means = df_byclass.mean()
     covs = df_byclass.cov()
     classes = df[class_column].unique()
-    with open(out_path, mode='w') as outfile:
-        outfile.write("CLASS1;CLASS2;DISTANCE\n")
-        for (class1, class2) in combinations(classes, r=2):
-            mu1, mu2 = means.loc[class1], means.loc[class2]
-            sigma1, sigma2 = covs.loc[class1], covs.loc[class2]
-            m = _jeffreys_matusita_distance(mu1, sigma1, mu2, sigma2)
-            outfile.write(f"{class1};{class2};{m}\n")
+    data = []
+    data_complete = []
+    for (class1, class2) in combinations(classes, r=2):
+        mu1, mu2 = means.loc[class1], means.loc[class2]
+        sigma1, sigma2 = covs.loc[class1], covs.loc[class2]
+        m = _jeffreys_matusita_distance(mu1, sigma1, mu2, sigma2) * np.sqrt(2)
+        data.append([class1, class2, m])
+        data_complete.append([class1, class2, m])
+        data_complete.append([class2, class1, m])
+    for class_ in classes:
+        data_complete.append([class_,class_,2.0])
+    matusita_df = pd.DataFrame(data, columns=['CLASS1', 'CLASS2', 'DISTANCE'])
+    matusita_df.sort_values(by=["DISTANCE"], ascending=True)
+    matusita_df.to_csv(join(out_path,"matusita.csv"), index=False)
+    matusita_df_complete = pd.DataFrame(data_complete, columns=['CLASS1', 'CLASS2', 'DISTANCE'])
+    matusita_df_complete = matusita_df_complete.pivot('CLASS1', 'CLASS2', 'DISTANCE')
+    sns.heatmap(matusita_df_complete, vmin=1.8, vmax=2, center=1.95, cmap="RdYlGn", square=True, cbar=False)
+    plt.savefig(join(out_path,"matusita.png"), bbox_inches="tight")
 
