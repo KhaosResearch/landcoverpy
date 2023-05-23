c056a6be3c33f3dca657d899c4206e93a4e669ba
ireneeromero <ireero99@gmail.com>
2023-05-23 14:23:04 +0200

    open forest and dense forest

--- src/landcoverpy/model_training.py
+++ src/landcoverpy/model_training.py
@@ -35,32 +35,11 @@ def _feature_reduction(
 
     return used_columns
 
-import geocoder
-
-
-def get_country(lat, lon):
-        # time.sleep(1)
-        # #Crear un objeto geolocalizador
-        # geolocator = Nominatim(user_agent="my_geocoder")
-        # location = geolocator.reverse(f"{lat}, {lon}", language='en')
-        # if location is not None:
-        #     return location.raw['address'].get('country',  '')
-        # else:
-        #     return None
-    location = geocoder.osm([lat, lon], method='reverse')
-    
-    if location.ok:
-        country = location.country
-        return country
-    else:
-        return None
-
-        
 
 
 
 
-def verificar_coordenadas(geodf, row):
+def get_country(geodf, row):
     latitude = row['latitude']
     longitude = row['longitude']
 
@@ -70,11 +49,17 @@ def verificar_coordenadas(geodf, row):
     
     return None
 
-def save_df_with_countries(df, gdf):
-    df['country'] = df.apply(lambda row: verificar_coordenadas(gdf, row), axis=1)
-    
-    df.to_csv("./data/dataset_postprocessed_with_country.csv")
-    return df
+
+def get_country1(lat, lon):
+        # Crear un objeto geolocalizador
+        geolocator = Nominatim(user_agent="my_geocoder")
+        
+        location = geolocator.reverse(f"{lat}, {lon}", exactly_one=True)
+        if location is not None:
+            return location.raw['address'].get('country',  '')
+        else:
+            return None
+        
 
 def train_model_land_cover(n_jobs: int = 2):
     """Trains a Random Forest model using a land cover dataset."""
@@ -85,21 +70,12 @@ def train_model_land_cover(n_jobs: int = 2):
     train_df = train_df.fillna(np.nan)
     train_df= train_df.dropna()
 
-    df_with_country = train_df.head(20).copy()
-
-
-
     gdf = gpd.read_file('./geojson/global.json')
-    
 
+    train_df['country'] = train_df.apply(lambda row: get_country(gdf, row), axis=1)
     
     
-    #Just run once to save the df
-    df_with_country = save_df_with_countries(train_df, gdf)
-    
-    #df_with_country = pd.read_csv("./data/dataset_postprocessed_with_country.csv")
-
-    train_grouped = df_with_country.groupby("country").apply(lambda x: x.reset_index(drop=True))
+    train_grouped = train_df.groupby("country").apply(lambda x: x.reset_index(drop=True))
 
     y_train_data = train_grouped["class"]
     x_train_data = train_grouped.drop(
@@ -140,11 +116,87 @@ def train_model_land_cover(n_jobs: int = 2):
         # Realiza las predicciones para el país actual utilizando tu modelo entrenado
         y_pred_pais = clf.predict(X_country)
 
-        confusion_image_filename = f"./img/confusion_matrix_{country}.png"
+        confusion_image_filename = f"./img_land/confusion_matrix_{country}.png"
 
         compute_confusion_matrix(y_pred_pais, y_country, labels, country, out_image_path=confusion_image_filename)
         
 
+def train_model_forest(use_open_forest: bool = False ,n_jobs: int = 2):
+    """Trains a Random Forest model using a forest dataset."""
 
+
+    train_df = pd.read_csv("./data/dataset_forests.csv")
+
+    train_df = train_df.replace([np.inf, -np.inf], np.nan)
+    train_df = train_df.fillna(np.nan)
+    train_df = train_df.dropna()
+    
+    #train_df = train_df.head(20).copy()
+    
+    
+    if use_open_forest:
+        print("si")
+
+        train_df = train_df[train_df["class"] == "openForest"]
+        equivalents = {4: 'Conifers', 5: 'Leafy', 6: 'Mixed', 0: 'Others'}
+        train_df['forest_type'] = train_df['forest_type'].replace(equivalents)
+    else:
+        print("no")
+        train_df = train_df[train_df["class"] != "openForest"]
+        equivalents = {1: 'Conifers', 2: 'Leafy', 3: 'Mixed', 0: 'Other'}
+        train_df['forest_type'] = train_df['forest_type'].replace(equivalents)
+        
+    gdf = gpd.read_file('./geojson/global.json')
+    
+    #train_df['country'] = train_df.apply(lambda i: get_country1(i['latitude'], i['longitude']), axis=1)
+    train_df['country'] = train_df.apply(lambda row: get_country(gdf, row), axis=1)
+    train_grouped = train_df.groupby("country").apply(lambda x: x.reset_index(drop=True))
+    
+    y_train_data = train_grouped["forest_type"]
+    x_train_data = train_grouped.drop(
+        [
+            "class",
+            "latitude",
+            "longitude",
+            "spring_product_name",
+            "autumn_product_name",
+            "summer_product_name",
+            "forest_type",
+            "country"
+        ],
+        axis=1,
+    )
+
+    used_columns = _feature_reduction(x_train_data, y_train_data)
+
+    reduced_x_train_data = train_grouped[used_columns]
+    X_train, X_test, y_train, y_test = train_test_split(
+        reduced_x_train_data, y_train_data, test_size=0.15
+    )
+
+    # Train model
+    clf = RandomForestClassifier(n_jobs=n_jobs)
+    X_train = X_train.reindex(columns=used_columns)
+    
+    clf.fit(X_train, y_train)
+
+    labels = y_train_data.unique()
+    
+    print(X_train)
+    for country, df_test_country in X_test.groupby("country"):
+            # Obtén las características (X) y las etiquetas (y) para el país actual
+            X_country = df_test_country
+            y_country = y_test[df_test_country.index]
+
+            
+            # Realiza las predicciones para el país actual utilizando tu modelo entrenado
+            y_pred_pais = clf.predict(X_country)
+
+            confusion_image_filename = f"./img_forest/confusion_matrix_{country}.png"
+
+            compute_confusion_matrix(y_pred_pais, y_country, labels, country, out_image_path=confusion_image_filename)
 
 train_model_land_cover(2)
+
+#train_model_forest(True, 2)
+
