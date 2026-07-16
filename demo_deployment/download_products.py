bf86768d255b286816191300134b0d7da63c1ebc
xavigc16 <fgarciacobo@uma.es>
2026-07-16 06:55:35 +0000

    fix(download): run seasonal pipeline once

--- demo_deployment/download_products.py
+++ demo_deployment/download_products.py
@@ -1,13 +1,8 @@
-from datetime import datetime, timedelta
 from ds_download.download_using_sentinel_api import download_product_using_sentinel_api
-import json
 import os
 from landcoverpy.utilities.geometries import _group_validated_data_points_by_tile, _kmz_to_geojson, _csv_to_geojson
 
 def download_products():
-
-    with open(os.getenv("SEASONS_FILE"), "r") as f:
-        seasons = json.load(f)
 
     data_file = os.getenv("DB_FILE")
     if data_file.endswith(".kmz"):
@@ -29,14 +24,11 @@ def download_products():
 
     tiles_to_download = tiles_to_train.union(tiles_to_predict)
 
-    for season in seasons:
-        start_date = datetime.strptime(seasons[season]["start"], "%Y-%m-%d")
-        end_date = datetime.strptime(seasons[season]["end"], "%Y-%m-%d")
-
-        for tile in tiles_to_download:
-            print(f"Processing {tile} for season {season}")
-            download_product_using_sentinel_api(False, True, start_date, end_date, tile_id=tile)
+    #for tile in tiles_to_download:
+    tile = "30STG"
+    print(f"Processing {tile} for all seasons")
+    download_product_using_sentinel_api(False, False, seasons_path=os.getenv("SEASONS_FILE"), tile_id=tile, run_seasonal_pipeline=True, quantize_rasters=True)
 
 
-
-            
+if __name__ == "__main__":
+    download_products()
